2d3736eda94557a5bdcef4fbcab57e59a3925c41
Thomas Jakobi <thomas.jakobi@partout.info>
2016-12-19 11:09:44 +0100

    New translations

---
+++ de/core/components/superboxselect/lexicon/de/default.inc.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Default Lexicon Entries for SuperBoxSelect
+ *
+ * @package superboxselect
+ * @subpackage lexicon
+ */
+
+$_lang['superboxselect'] = 'SuperBoxSelect';
+
+$_lang['superboxselect.allowedUsergroups'] = 'Allowed Usergroups';
+$_lang['superboxselect.allowedUsergroups_desc'] = 'Comma separated list of allowed usergroups';
+$_lang['superboxselect.deniedUsergroups'] = 'Denied Usergroups';
+$_lang['superboxselect.deniedUsergroups_desc'] = 'Comma separated list of denied usergroups';
+$_lang['superboxselect.fieldTpl'] = 'Field Template';
+$_lang['superboxselect.fieldTpl_desc'] = 'Field Template for the SuperBoxSelect (could contain html tags). Default: <code>{title} ({id})</code>';
+$_lang['superboxselect.maxElements'] = 'Max. Elements';
+$_lang['superboxselect.maxElements_desc'] = 'Maximum number of Elements in the list. 0 means no limit';
+$_lang['superboxselect.pageSize'] = 'Page Size';
+$_lang['superboxselect.pageSize_desc'] = 'If the page size is greater than 0, a pagination is displayed in the footer of the dropdown list.';
+$_lang['superboxselect.selectPackage'] = 'Package';
+$_lang['superboxselect.selectPackage_desc'] = 'Folder name of a package, where the \'types\' getlists processor are retrieved from.';
+$_lang['superboxselect.selectType'] = 'Type';
+$_lang['superboxselect.selectType_desc'] = 'Content type of the dropdown list.';
+
+$_lang['superboxselect.maxElements_label'] = 'max. {maxElements}';
+$_lang['superboxselect.maxElements_msg'] = 'Reached maximum number of elements.';
+
+$_lang['superboxselect.resources'] = 'Resources';
+$_lang['superboxselect.users'] = 'Users';
+
+$_lang['setting_superboxselect.advanced'] = 'Advanced';
+$_lang['setting_superboxselect.advanced_desc'] = 'Display advanced TV settings (i.e. Package and Field Template).';
+$_lang['setting_superboxselect.debug'] = 'Debug';
+$_lang['setting_superboxselect.debug_desc'] = 'Log debug informations in MODX error log.';
