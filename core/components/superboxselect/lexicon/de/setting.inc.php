8a4908704777ce3e6b14eb11815fab0edeee8f44
Thomas Jakobi <thomas.jakobi@partout.info>
2019-02-17 16:30:29 +0100

    New translations setting.inc.php (German)

---
+++ core/components/superboxselect/lexicon/de/setting.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Setting Lexicon Entries for SuperBoxSelect
+ *
+ * @package superboxselect
+ * @subpackage lexicon
+ */
+
+$_lang['setting_superboxselect.advanced'] = 'Erweitert';
+$_lang['setting_superboxselect.advanced_desc'] = 'Erweiterte TV-Einstellungen anzeigen (z.B. Package und Feld Template).';
+$_lang['setting_superboxselect.debug'] = 'Debug';
+$_lang['setting_superboxselect.debug_desc'] = 'Debug-Informationen im MODX Fehlerprotokoll ausgeben.';
