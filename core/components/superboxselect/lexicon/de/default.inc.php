82cede1065a6b8fdf27ef8b0887d9a7a4f5d7f4a
Thomas Jakobi <buero@treehillstudio.de>
2021-04-03 15:35:07 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (24 of 24 strings)
    
    Translation: MODX SuperBoxSelect/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-superboxselect/standard/de/

--- core/components/superboxselect/lexicon/de/default.inc.php
+++ core/components/superboxselect/lexicon/de/default.inc.php
@@ -5,32 +5,27 @@
  * @package superboxselect
  * @subpackage lexicon
  */
-
 $_lang['superboxselect'] = 'SuperBoxSelect';
-
 $_lang['superboxselect.allowedUsergroups'] = 'Erlaubte Benutzergruppen';
-$_lang['superboxselect.allowedUsergroups_desc'] = 'Komma-getrennte Liste von erlaubten Benutzergruppen';
-$_lang['superboxselect.deniedUsergroups'] = 'Abgelehnte Benutzergruppen';
-$_lang['superboxselect.deniedUsergroups_desc'] = 'Komma-getrennte Liste von abgelehnten Benutzergruppen';
+$_lang['superboxselect.allowedUsergroups_desc'] = 'Kommagetrennte Liste der erlaubten Benutzergruppen';
+$_lang['superboxselect.deniedUsergroups'] = 'Verweigerte Benutzergruppen';
+$_lang['superboxselect.deniedUsergroups_desc'] = 'Kommagetrennte Liste der verweigerten Benutzergruppen';
 $_lang['superboxselect.fieldTpl'] = 'Feld Template';
-$_lang['superboxselect.fieldTpl_desc'] = 'Feld Template für die SuperBoxSelect (kann Html-Tags enthalten). Standard: <code>{title} ({id})</code>';
+$_lang['superboxselect.fieldTpl_desc'] = 'Feld Template für die SuperBoxSelect (kann html-Tags enthalten). Standard: <code>{title} ({id})</code>';
 $_lang['superboxselect.maxElements'] = 'Max. Elemente';
-$_lang['superboxselect.maxElements_desc'] = 'Maximale Anzahl von Elementen in der Liste (0 bedeutet keine Einschränkung)';
+$_lang['superboxselect.maxElements_desc'] = 'Maximale Anzahl von Elementen in der Liste (0 bedeutet keine Begrenzung)';
 $_lang['superboxselect.pageSize'] = 'Seitengröße';
-$_lang['superboxselect.pageSize_desc'] = 'Wenn die Seitengröße größer als 0 ist, wird eine Paginierung in der Fußzeile der Dropdown-Liste angezeigt.';
+$_lang['superboxselect.pageSize_desc'] = 'Wenn die Seitengröße größer als 0 ist, wird in der Fußzeile der Dropdown-Liste eine Paginierung angezeigt.';
 $_lang['superboxselect.selectPackage'] = 'Package';
-$_lang['superboxselect.selectPackage_desc'] = 'Name des Ordners eines Packages, in dem sich die "Typ" List Prozessoren befinden.';
+$_lang['superboxselect.selectPackage_desc'] = 'Name des Ordners eines Packages, in dem sich die \'Typ\' List Prozessoren befinden.';
 $_lang['superboxselect.selectType'] = 'Typ';
 $_lang['superboxselect.selectType_desc'] = 'Inhaltstyp der Dropdown-Liste.';
 $_lang['superboxselect.stackItems'] = 'Elemente Stapeln';
-$_lang['superboxselect.stackItems_desc'] = 'Wenn aktiviert, werden die SuperBoxSelect-Elemente gestapelt angezeigt. Standardmäßig werden die Elemente in der Reihe angezeigt.';
+$_lang['superboxselect.stackItems_desc'] = 'Wenn diese Option aktiviert ist, werden die SuperBoxSelect-Elemente einzeln pro Zeile gestapelt. Standardmäßig werden die Elemente inline angezeigt.';
 $_lang['superboxselect.valueField'] = 'Wert Feld';
 $_lang['superboxselect.valueField_desc'] = 'Der Name des Feldes, dessen Wert in der Template-Variable gespeichert wird. Standard: <code>id</code>';
-
 $_lang['superboxselect.maxElements_label'] = 'max. {maxElements}';
 $_lang['superboxselect.maxElements_msg'] = 'Maximale Anzahl von Elementen erreicht.';
-
 $_lang['superboxselect.resources'] = 'Ressourcen';
 $_lang['superboxselect.users'] = 'Benutzer';
-
 $_lang['superboxselect.err_wrong_package'] = 'Das Package existiert nicht!';
