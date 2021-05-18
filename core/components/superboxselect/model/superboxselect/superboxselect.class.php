5c2498a21fa1179874ee5edc55487bfdf9fe64e9
Thomas Jakobi <thomas.jakobi@partout.info>
2021-05-18 19:59:04 +0200

    Normalize code

--- core/components/superboxselect/model/superboxselect/superboxselect.class.php
+++ core/components/superboxselect/model/superboxselect/superboxselect.class.php
@@ -159,7 +159,7 @@ class SuperBoxSelect
         }
 
         $this->modx->controller->addHtml('<script type="text/javascript">'
-            . 'SuperBoxSelect.config = ' . json_encode($this->config, JSON_PRETTY_PRINT) . ';'
+            . 'SuperBoxSelect.config = ' . json_encode($this->config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . ';'
             . '</script>');
     }
 
