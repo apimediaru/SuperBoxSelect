d912c6abb0486c5dc3f35c46a0e6318f22a5d19c
Thomas Jakobi <thomas.jakobi@partout.info>
2021-11-16 15:09:58 +0100

    Update lexicon

--- core/components/superboxselect/lexicon/en/setting.inc.php
+++ core/components/superboxselect/lexicon/en/setting.inc.php
@@ -9,4 +9,4 @@
 $_lang['setting_superboxselect.advanced'] = 'Advanced';
 $_lang['setting_superboxselect.advanced_desc'] = 'Display advanced TV settings (i.e. Package and Field Template).';
 $_lang['setting_superboxselect.debug'] = 'Debug';
-$_lang['setting_superboxselect.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_superboxselect.debug_desc'] = 'Log debug information in the MODX error log.';
